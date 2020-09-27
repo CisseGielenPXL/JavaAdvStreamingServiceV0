7fd139fc51310c4cbf15eab4ecba8683e195c0e6
CisseGielenPXL <11802936@student.pxl.be>
2020-09-27 14:57:27 +0200

    Account properties + getters and setters

--- src/main/java/be/pxl/ja/streamingservice/model/Account.java
+++ src/main/java/be/pxl/ja/streamingservice/model/Account.java
@@ -1,4 +1,27 @@
 package be.pxl.ja.streamingservice.model;
 
+import java.util.List;
+
 public class Account {
+    private String email;
+    private String password;
+    private PaymentInfo paymentInfo;
+    private StreamingPlan streamingPlan;
+    private List<Profile> profiles;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPaymentInfo(PaymentInfo paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public void setStreamingPlan(StreamingPlan streamingPlan) {
+        this.streamingPlan = streamingPlan;
+    }
 }
